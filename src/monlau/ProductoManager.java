c914465b8ea00422e637c0c60b5a616c6ab28ac8
Lock6903 <eric7pd@gmail.com>
2023-03-23 20:10:49 +0100

    Initial commit

---
+++ src/monlau/ProductoManager.java
@@ -0,0 +1,21 @@
+
+package monlau;
+
+import monlau.dao.ProductoDAO;
+import monlau.dao.ProductoDAOImpl;
+import monlau.model.Producto;
+
+public class ProductoManager {
+    public static void main(String[] args) {
+        ProductoDAO producto =new ProductoDAOImpl();
+        
+        //agregar nuevo producto
+        producto.insert(new Producto(1,"Pollo",10.0));
+        
+        //obtener el producto con el ID = 100
+        Producto p = producto.read(200);
+        System.out.println(p);
+        //producto.delete(200);
+        //producto.update();
+    }
+}
